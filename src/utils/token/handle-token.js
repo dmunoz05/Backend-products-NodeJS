26e8998478c87e9067bc418fdbf9b929a6224c6f
= <daniel20025febrero29@gmail.com>
2024-02-28 17:30:42 -0500

    Delete comment token

--- src/utils/token/handle-token.js
+++ src/utils/token/handle-token.js
@@ -1,13 +1,5 @@
 import jwt from 'jsonwebtoken'
 
-// const secret = 'myCat'
-// const tokenValue = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInJvbGUiOiJjdXN0b21lciIsImlhdCI6MTcwOTE0ODMzNH0.Jpf4KnjkbwAfKz3FHbQV6flf394fKGx6QZZM-cBhE7E'
-
-// const payload = {
-//     sub: 1,
-//     role: 'customer'
-// }
-
 export function generateToken(payload) {
     return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '15min'})
 }
