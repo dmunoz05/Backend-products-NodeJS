497c0be449540a9406aa77117c4c934bb8829e64
= <daniel20025febrero29@gmail.com>
2024-02-28 22:36:17 -0500

    Details in code

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 import express from 'express'
 import dotenv from './config.js'
-import passport from 'passport'
+import cors from 'cors'
 // import { LocalStrategy } from './src/utils/strategies/local.strategy.js'
 
 dotenv.config()
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200
+}
 
 const app = express()
 
 app.use(express.json())
 
-app.use(passport.initialize());
-
-// passport.use(new LocalStrategy())
+app.use(cors(corsOptions))
 
 export default app
